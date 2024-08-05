ec2ae645d7987cbf034e07f1b6e98d6bd3da9a65
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-05 08:38:45 +0530

    index.js

--- MVC/index.js
+++ MVC/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoutes');
-const db = require('./config/db');
 
 const app = express();
-
-app.use(bodyParser.json());
+app.use(express.json())
 app.use('/', productRoutes);
 
 app.listen(8530, () => {
