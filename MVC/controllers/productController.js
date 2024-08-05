b8dce3888ea8ed68c37207d7ea0424797bb0bfbc
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-05 08:34:44 +0530

    productController.js

--- MVC/controllers/productController.js
+++ MVC/controllers/productController.js
@@ -1,41 +1,24 @@
 const Product = require('../models/product');
 
-module.exports.createProduct = async (req, res) => {
-    try {
-        const product = new Product(req.body);
-        await product.save();
-        res.status(201).send(product);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
-module.exports.getProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).send(products);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+const getProducts = async (req, res) => {
+    let data = await User.find()
+    res.send(data)
 }
-module.exports.updateProduct = async (req, res) => {
-    try {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!product) {
-            return res.status(404).send();
-        }
-        res.status(200).send(product);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
-module.exports.deleteProduct = async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
-        if (!product) {
-            return res.status(404).send();
-        }
-        res.status(200).send(product);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
+
+const createProduct = async (req, res) => {
+    let data = await User.create(req.body)
+    res.status(201).send(data)
+}
+const updateProduct = async (req, res) => {
+    let {id} = req.params;
+    let data = await User.findByIdAndUpdate(id, req.body);
+    res.send(data);
+}
+const deleteProduct = async (req, res) => {
+    let {id} = req.params;
+     let data = await User.findByIdAndDelete(id);
+    res.send(data);
+}
+
+
+module.exports = { getProducts,createProduct ,updateProduct , deleteProduct }
