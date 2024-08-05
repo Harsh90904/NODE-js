08a8eb4c336ae4c43496d50361f54148f5d4a4d2
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-05 10:02:20 +0530

    db.js

--- MVC/config/db.js
+++ MVC/config/db.js
@@ -1,6 +1,8 @@
+
 const mongoose = require("mongoose")
+require("dotenv").config()
 const dbConnect = async () => {
-    await mongoose.connect("mongodb://localhost:27017/product")
+    await mongoose.connect(process.env.DB_URL)
     console.log("connect to the database");
 }
 module.exports = dbConnect
