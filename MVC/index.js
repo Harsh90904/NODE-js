1df2c2c3d8ed6dfc52fe5df0428471ede7e84e45
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-05 09:32:11 +0530

    index.js

--- MVC/index.js
+++ MVC/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const dbConnect = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
-app.use(express.json())
-app.use('/', productRoutes);
+app.use(express.json());
+app.use('/:id', productRoutes);
 
-app.listen(8530, () => {
-    console.log(`Server is running on port 8560`);
+app.listen(8030, () => {
+    console.log("http://localhost:8030 is running");
+    dbConnect();
 });
